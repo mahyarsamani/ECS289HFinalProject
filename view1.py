61255b7a2b5d7b0dc040b252d3b49576735b8721
mbabaie <43761552+mbabaie@users.noreply.github.com>
2020-12-11 02:08:31 -0800

    Add files via upload

--- view1.py
+++ view1.py
@@ -260,6 +260,136 @@ app.layout = html.Div([
     
   #####################################################################
    # --------------------- Div View 3 ---------------------
+     html.Div([
+		# ---------- Div General settings
+		html.Div([
+            
+            html.Div([
+            dcc.Textarea(id="V3_Divider",
+                value = ['\n-----------------\n---> Views 3 <---\n-----------------\n'],
+                readOnly = True,
+                style={'width': '99%','backgroundColor': '#f8f8f8', 'justify': 'center', 'display': 'flex','resize': 'none','border':'none', 'height': 150, 'color': 'green', 'font-size':'25px'}),
+            ]),
+            
+            html.Div([
+            dcc.Textarea(id="V3_txt1",
+                value = ['Select Desired Group(s):'],
+                readOnly = True,
+                style={'width': '99%','backgroundColor': '#f8f8f8', 'justify': 'center', 'display': 'flex','resize': 'none','border':'none'}),
+            ]),
+            
+            html.Div([
+                dcc.Dropdown(
+                    id='V3_groupname',
+                    options=[{'label': i, 'value': i} for i in cat_cols],
+                    value=cat_cols[0]
+                )
+            ], style={'width': '100%', 'float': 'left', 'display': 'inline-block'}),
+            
+            html.Div([
+            dcc.Textarea(id="V3_txt2",
+                value = ['Select Desired Numerical Feature(s):'],
+                readOnly = True,
+                style={'width': '99%','backgroundColor': '#f8f8f8', 'justify': 'center', 'display': 'flex','resize': 'none','border':'none'}),
+            ]),
+            
+            html.Div([
+                dcc.Dropdown(
+                    id='V3_featconti',
+                    options=[{'label': i, 'value': i} for i in num_cols],
+                    value=num_cols[0:5],
+                    multi=True,
+                )
+            ], style={'width': '100%', 'float': 'left', 'display': 'inline-block'}),
+            
+            html.Div([
+            dcc.Textarea(id="V3_txt3",
+                value = ['Select Desired Categorical Feature(s):'],
+                readOnly = True,
+                style={'width': '99%','backgroundColor': '#f8f8f8', 'justify': 'center', 'display': 'flex','resize': 'none','border':'none'}),
+            ]),
+            
+            html.Div([
+                dcc.Textarea(id="V3_txtyerrng",
+                    value = ['Select Desired year range:'],
+                    readOnly = True,
+                    style={'width': '99%','backgroundColor': '#f8f8f8', 'justify': 'center', 'display': 'flex','resize': 'none','border':'none'}),
+            ]),
+            
+            
+            html.Div([
+                dcc.Checklist(
+                    id='V3_Preproc_Flags',
+                    options=[{'label': 'Normalize', 'value': 'norm_flag'},
+                             {'label': 'Enable PCA', 'value': 'pca_flag'}],
+                    value=['norm_flag','pca_flag']
+                )  
+            ]),
+            
+            html.Div([
+                dcc.Input(
+                    id='V3_pcacompo', type='text', 
+                    value = ['Select the number of components between '+ str(2) + ' and ' + str(10)+':'],
+                    placeholder="",readOnly = False,
+                    style={'width': '99%','backgroundColor': '#f8f8f8', 'float': 'left', 'display': 'block','border':'none','whiteSpace': 'pre-line','height': 30}
+                )
+            ]),
+            html.Div([
+                dcc.Input(
+                    id="V3_pcaCompNum_txt", type="number", placeholder="", value=5,
+                    min=2, max=10, step=1,
+                )
+            ]),
+            
+            html.Div([
+                dcc.Textarea(
+                    id="V3_datapoints",
+                    value = ['Data Points:'],
+                    readOnly = True,
+                    style={'width': '99%', 'float': 'Center', 'display': 'inline-block','whiteSpace': 'pre-line','resize': 'none','height': 150}
+                ),
+            ]),
+            
+            html.Div([
+                dcc.Textarea(
+                    id="V3_txt4",
+                    value = ['Select percentage of datapoints used for testing and evaluation in the slider below:'],
+                    readOnly = True,
+                    style={'width': '100%', 'justify': 'center', 'display': 'flex','resize': 'none','border':'none','backgroundColor': '#f8f8f8'}),
+            ]),
+            
+            html.Div(
+                dcc.Slider(
+                    id='V3_testproport',
+                    min = 10,
+                    max = 50,
+                    step=1,
+                    value=30,
+                    marks={str(i): str(i) for i in range(10,50)}
+                ), style={'width': '99%', 'float': 'right', 'padding': '0px 20px 20px 20px','whiteSpace': 'pre-line'}
+            ),
+            
+            
+            html.Div(
+                dcc.Input(
+                    id='V3_accu', type='text', 
+                    value = ['Confusion Table:'],
+                    placeholder="",readOnly = True,
+                    style={'width': '99%','backgroundColor': '#f8f8f8', 'float': 'left', 'display': 'block','border':'none','whiteSpace': 'pre-line','height': 30})
+                ),
+			# ---------- graph
+			html.Div([
+                dcc.Graph(
+                    id='V3_conftable_fig'
+                )
+            ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),
+	
+       
+		])
+		
+		# ---------- Div Axis choice
+		
+	])
    
 ],style={'backgroundColor': '#f8f8f8'},)
 
@@ -554,7 +684,86 @@ def update_V1Tile_lvl5_val(V1_Var5Tile):
    return df1[V1_Var5Tile].unique()[0:2]
 
 
+#-------------------------------- View 3 Callbacks
+@app.callback(
+    dash.dependencies.Output(component_id = 'V3_datapoints', component_property = 'value'),
+    dash.dependencies.Output(component_id = 'V3_conftable_fig', component_property = 'figure'),
+    [dash.dependencies.Input(component_id = 'V3_groupname', component_property = 'value'),
+      dash.dependencies.Input(component_id = 'V3_featconti', component_property = 'value'),
+      dash.dependencies.Input(component_id = 'V3_Preproc_Flags', component_property = 'value'),
+      dash.dependencies.Input(component_id = 'V3_pcaCompNum_txt', component_property = 'value'),
+      dash.dependencies.Input(component_id = 'V3_testproport', component_property = 'value')
+      ])
+def update_graph(grname, contfeat, preprocflag, Component_num, testproport):
+    
+    #----------------------- Select groups and year ranges
+    Catlabel = grname
+    ML_Cols = np.concatenate([[grname],contfeat])
 
+    grname = df1[Catlabel].unique() 
+    # df_ML = df1[(df1['iyear']>year_range[0]) & (df1['iyear']<year_range[1])]
+    # df_ML = df_ML[df_ML['gname'].isin(grname)]
+    df_ML = df1[ML_Cols]
+    df_ML = df_ML.fillna(df_ML.mean())
+    
+    print(df_ML)
+
+    df_Cont = pd.DataFrame(df_ML, columns = contfeat)
+    
+    labels = df_ML[Catlabel]
+    
+    
+    if 'norm_flag' in preprocflag:
+     	# ------------ Normalize
+     	sc = StandardScaler()
+     	df_Cont = sc.fit_transform(df_Cont)
+    
+    if 'pca_flag' in preprocflag:
+     	# ------------ PCA
+     	pca = PCA(n_components = Component_num)
+     	df_Cont = pca.fit_transform(df_Cont)
+     	explained_variance = pca.explained_variance_ratio_
+
+    
+    feats = df_Cont
+    
+
+    X_train, X_test, y_train, y_test = train_test_split(feats, labels, test_size=testproport/100, random_state=0)
+
+    datapoints = df_ML.groupby([Catlabel]).size().reset_index(name="idx N")
+    datapoints = pd.DataFrame(datapoints, columns = ['idx N',Catlabel])
+
+    
+    datapoints_str = ['Number of datapoints: \n'
+                   + str(datapoints)]
+    
+    
+    # ------------ Random Forest classifier 
+    classifier = RandomForestClassifier(max_depth=2, random_state=0)
+    classifier.fit(X_train, y_train)
+
+    y_pred = classifier.predict(X_test)
+    
+    cm = confusion_matrix(y_test, y_pred)
+
+    import plotly.figure_factory as ff
+
+    # print(cm)
+    # print(grname[len(grname)::-1])
+    fig = ff.create_annotated_heatmap(cm, 
+                                      font_colors=['black'], hoverinfo='text',
+                                      colorscale='Viridis')
+    # fig = ff.create_annotated_heatmap(cm[len(grname)::-1].T, 
+    #                                   x = grname,
+    #                                   y = grname[len(grname)::-1],
+    #                                   font_colors=['black'], hoverinfo='text',
+    #                                   colorscale='Viridis')
+    fig.update_layout(title_text= 'Accuracy = '+ str(accuracy_score(y_test, y_pred)))
+    
+    
+    return (datapoints_str
+            , fig
+    )
 
 
 if __name__ == '__main__':
